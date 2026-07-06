50afbaf43713e7ba867bcf0c7e5ac58f89a79bde
Hacker <phoniegiwme@gmail.com>
2026-07-06 09:23:15 +0300

    Raise float accessibility speech container on mobile screens to prevent overlapping with nav bar

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const BASE_PATH = self.location.pathname.substring(0, self.location.pathname.lastIndexOf('/') + 1);
 
-const CACHE_VERSION = 'dkut-hostel-v4';
+const CACHE_VERSION = 'dkut-hostel-v5';
 const DATA_CACHE = 'dkut-hostel-data-v2';
 
 const STATIC_ASSETS = [
